d69eb5705283797c778be48c7b47b2b5573cbe2a
oskop123 <oswierczek99@gmail.com>
2020-04-20 22:42:17 +0200

    initial commit

--- source/MovingAverageCrossStrategy.py
+++ source/MovingAverageCrossStrategy.py
@@ -10,24 +10,25 @@ class MovingAverageCrossStrategy:
     short_window - Lookback period for short moving average.
     long_window - Lookback period for long moving average."""
 
-    short_window = 40
-    long_window = 100
-
-    def __init__(self, symbol, bars):
+    def __init__(self, symbol, bars, short_window, long_window):
         self.symbol = symbol
         self.bars = bars
+        self.short_window = short_window
+        self.long_window = long_window
         self.signals = pd.DataFrame(index=bars.index)
-        self.signals['signal'] = 0.0
 
     def calculate(self):
+        self.signals['signal'] = 0.0
         self.signals['short_mavg'] = self.bars['Close'].rolling(
-            window=MovingAverageCrossStrategy.short_window, min_periods=1, center=False).mean()
+            window=self.short_window, min_periods=1, center=False).mean()
 
         self.signals['long_mavg'] = self.bars['Close'].rolling(
-            window=MovingAverageCrossStrategy.long_window, min_periods=1, center=False).mean()
+            window=self.long_window, min_periods=1, center=False).mean()
 
-        self.signals['signal'][MovingAverageCrossStrategy.short_window:] = np.where(
-            self.signals['short_mavg'][MovingAverageCrossStrategy.short_window:]
-            > self.signals['long_mavg'][MovingAverageCrossStrategy.short_window:], 1.0, 0.0)
+        self.signals['signal'][self.short_window:] = np.where(
+            self.signals['short_mavg'][self.short_window:]
+            > self.signals['long_mavg'][self.short_window:], 1.0, 0.0)
 
         self.signals['positions'] = self.signals['signal'].diff()
+
+        return self.signals
