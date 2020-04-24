7e76a6aa1bcb50a283ab145c2db6264e4d9616e4
Adam Ksiezyk <adam.ksiezyk@gmail.com>
2020-04-24 17:53:41 +0200

    Modified classes to functions

--- Portfolio.py
+++ Portfolio.py
@@ -1,37 +1,40 @@
-import datetime
 import pandas as pd
-import pandas_datareader as pdr
 import numpy as np
 import matplotlib.pyplot as plt
 
-class Portfolio:
-    """Keeps track of the portfolio of a stock based on signals passed by a strategy
+def portfolio(symbol, signals, initial_capital):
+    """Create a portfolio based on given: stock symbol, signals from a strategy and initial capital.
+    The portfolio contains: stocks values, signals, holdings, available cash, total cash and returns %."""
+    # Create data frame for current positions
+    positions = pd.DataFrame(index=signals.index).fillna(0.0)
+    positions[symbol] = 100*signals['signal']
+    # Create portfolio
+    portfolio = positions.multiply(signals['price'], axis=0)
+    # Add signals
+    portfolio['signal'] = signals['positions']
+    # Add the buy and sell shares commands
+    pos_diff = positions.diff()
+    # Add holdings
+    portfolio['holdings'] = positions.multiply(signals['price'], axis=0).sum(axis=1)
+    # Add cash
+    portfolio['cash'] = initial_capital - pos_diff.multiply(signals['price'], axis=0).sum(axis=1).cumsum()
+    # Add total amount of cash
+    portfolio['total'] = portfolio['cash'] + portfolio['holdings']
+    # Add % change
+    portfolio['returns'] = portfolio['total'].pct_change().fillna(0.0)
+    return portfolio
 
-    Requires:
-    symbol - A stock symbol
-    bars - A DataFrame of bars for a symbol set.
-    signals - A pandas DataFrame of signals (1, 0, -1) for each symbol.
-    initial_capital - The amount in cash at the start of the portfolio."""
-
-    def __init__(self, symbol, signals, initial_capital):
-        # Initialize the variables
-        self.symbol = symbol
-        self.bars = bars
-        self.signals = signals
-        self.initial_capital = float(initial_capital)
-        self.positions = self.generate_positions()
-
-    def generate_positions(self):
-        # Generate positions from signals
-        positions = pd.DataFrame(index=self.signals.index).fillna(0.0)
-        positions[self.symbol] = 100*signals['signal']
-        return positions
-
-    def calculate(self):
-        portfolio = self.positions*self.bars['Close']
-        pos_diff = self.positions.diff()
-        portfolio['holdings'] = (self.positions*self.bars['Close']).sum(axis=1)
-        portfolio['cash'] = self.initial_capital - (pos_diff*self.bars['Close']).sum(axis=1).cumsum()
-        portfolio['total'] = portfolio['cash'] + portfolio['holdings']
-        portfolio['returns'] = portfolio['total'].pct_change()
-        return portfolio
+def portfolio_plot(portfolio):
+    """"Plot the given portfolio"""
+    # Create figure
+    fig = plt.figure()
+    # Define Y-axis
+    ax1 = fig.add_subplot(111, ylabel='Portfolio value')
+    # Plot total value
+    portfolio['total'].plot(ax=ax1, color='r', lw=2.)
+    # Plot entry and points
+    ax1.plot(portfolio.loc[portfolio.signal == 1.0].index, portfolio['total'][portfolio.signal == 1.0], '^',    markersize=10, color='m')
+    # Plot exit points
+    ax1.plot(portfolio.loc[portfolio.signal == -1.0].index, portfolio['total'][portfolio.signal == -1.0], 'v', markersize=10, color='k')
+    # Show plot
+    plt.show()
