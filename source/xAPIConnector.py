1f714186642826a576cb6bafc8b995260208c760
oskop123 <oswierczek99@gmail.com>
2020-05-12 15:41:14 +0200

    merged

--- source/xAPIConnector.py
+++ source/xAPIConnector.py
@@ -4,12 +4,10 @@ import ssl
 import time
 from threading import Thread
 
-# default connection properites
+# connection properites
 DEFAULT_ADDRESS = 'xapi.xtb.com'
 DEFAULT_PORT = 5124
 DEFAULT_STREAMING_PORT = 5125
-
-# max connection tries
 API_MAX_CONN_TRIES = 3
 
 
@@ -26,7 +24,14 @@ class TransactionType(object):
 
 
 class JsonSocket(object):
+    """ Socket used for JSON communication."""
+
     def __init__(self, address, port, encrypt=False):
+        """ socket - socket object
+            _address - destination address (server)
+            _port - destination port (server)
+            _decoder - object used for decoding JSON
+            _receivedData - stores received data"""
         if encrypt:
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.socket = ssl.wrap_socket(sock)
@@ -37,8 +42,8 @@ class JsonSocket(object):
         self._decoder = json.JSONDecoder()
         self._receivedData = ''
 
-    # server connection
     def connect(self):
+        """ Connect to server. """
         for i in range(API_MAX_CONN_TRIES):
             try:
                 self.socket.connect((self._address, self._port))
@@ -49,18 +54,22 @@ class JsonSocket(object):
         raise Exception('Cannot connect to server')
 
     def _send_obj(self, obj):
-        msg = json.dumps(obj)  # serialize dict into JSON format
+        """ Send data to server in JSON format. """
+
+        # serialize dict into JSON format
+        msg = json.dumps(obj)
         if self.socket:
             msg = msg.encode('utf-8')
-            self.socket.sendall(msg)  # send data in JSON format
+            self.socket.sendall(msg)
         else:
             raise RuntimeError('socket connection broken')
 
-    def _read(self, bytes_size=4096):
+    def _read_obj(self):
+        """ Receive data from server. Returns Python dictionary."""
         if not self.socket:
             raise RuntimeError('socket connection broken')
         while True:
-            char = self.socket.recv(bytes_size).decode('utf-8')
+            char = self.socket.recv(4096).decode('utf-8')
             self._receivedData += char
             try:
                 (resp, size) = self._decoder.raw_decode(self._receivedData)
@@ -74,39 +83,44 @@ class JsonSocket(object):
                 continue
         return resp
 
-    def _read_obj(self):
-        msg = self._read()
-        return msg
-
     def close(self):
+        """ Close connection. """
         self.socket.close()
 
 
 class APIClient(JsonSocket):
+    """  Extension of JsonSocket for Request/Response Service. """
+
     def __init__(self, address=DEFAULT_ADDRESS, port=DEFAULT_PORT, encrypt=True):
         super(APIClient, self).__init__(address, port, encrypt)
         self.connect()
 
     def execute(self, dictionary):
+        """ Sends request. Returns response. """
         self._send_obj(dictionary)
         return self._read_obj()
+
+    def command_execute(self, command_name, arguments=None):
+        """ Sends request. Returns response. """
+        return self.execute(base_command(command_name, arguments))
 
     def disconnect(self):
         self.close()
 
-    def command_execute(self, command_name, arguments=None):
-        return self.execute(base_command(command_name, arguments))
-
 
 class APIStreamClient(JsonSocket):
+    """ Extension of JsonSocket for Streaming Service. """
+
     def __init__(self, address=DEFAULT_ADDRESS, port=DEFAULT_STREAMING_PORT,
                  encrypt=True, ss_id=None, tick_fun=None):
         super(APIStreamClient, self).__init__(address, port, encrypt)
-        self._ssId = ss_id  # stream session id - needed for streaming subscription
-        self._tickFun = tick_fun  # data processing function
+        """ _ssId -  stream session id 
+            _tickFun - data processing function
+            _running - loop condition used in _t
+            _t - thread used in data receiving process"""
+        self._ssId = ss_id
+        self._tickFun = tick_fun
         self.connect()
-
-        # Thread for reading streaming data
         self._running = True
         self._t = Thread(target=self._read_stream)
         self._t.start()
@@ -117,11 +131,12 @@ class APIStreamClient(JsonSocket):
             self._tickFun(msg)
 
     def disconnect(self):
-        self._running = False  # stop thread function
-        self._t.join()  # terminate thread
-        self.close()  # close connection
+        self._running = False
+        self._t.join()
+        self.close()
 
     def execute(self, dictionary):
+        """ Sends request. """
         self._send_obj(dictionary)
 
     def subscribe_price(self, symbol, interval):
@@ -132,13 +147,13 @@ class APIStreamClient(JsonSocket):
             self.subscribe_price(symbolX, interval)
 
 
-# return dictionary representing JSON objects
 def base_command(command_name, arguments=None):
+    """ Returns dictionary representing JSON object. """
     if arguments is None:
         arguments = dict()
     return dict([('command', command_name), ('arguments', arguments)])
 
 
-# return dictionary representing JSON login object
 def login_command(user_id, password, app_name=''):
+    """ Returns dictionary representing JSON login object. """
     return base_command('login', dict(userId=user_id, password=password, appName=app_name))
